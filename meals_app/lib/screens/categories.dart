c7b02ad76ea583284b1c45d0815656eb070befbf
Mikail Korkmaz <mikail-korkmaz@web.de>
2024-07-29 14:00:34 +0200

    adding & loading meals Data into a screen, adding cross-screen Navigation, passing Data to the Target Screen

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
-
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    ); // Navigator.push(context, route)
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +38,13 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()      Alternative
-          for(final category in availableCategories)
-          CategoryGridItem(category: category)
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
