e9bc137aea20e2c55719c18518227b1172d8831f
svote.io <devops@svote.io>
2024-08-05 14:50:44 +0200

    created a Provider with StateNotifier, using the FavoriteProvider and triggering a Notifier Method, making the code leaner

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -8,11 +8,9 @@ import 'package:meals_app/models/meal.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
     super.key,
-    required this.onToggleFavorite,
     required this.availableMeals,
   });
 
-  final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
@@ -25,7 +23,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealScreen(
           title: category.title,
           meals: filteredMeals,
-          onToggleFavorite: onToggleFavorite,
+          
         ),
       ),
     ); // Navigator.push(context, route)
